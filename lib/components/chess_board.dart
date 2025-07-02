a68d4fa5f97a8c44ed5f3217b99c486822559b1c
upahar windows <upaharlol@gmail.com>
2025-07-02 20:19:13 +0545

    Board Initialization done

---
+++ lib/components/chess_board.dart
@@ -0,0 +1,23 @@
+import 'package:chess/helper_methods/square_selector.dart';
+import 'package:flutter/material.dart';
+
+class ChessBoard extends StatefulWidget {
+  final int index;
+  final ChessPiece? piece;
+  const ChessBoard({super.key, required this.index, required this.piece});
+
+  @override
+  State<ChessBoard> createState() => _ChessBoardState();
+}
+
+class _ChessBoardState extends State<ChessBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: (widget.index % 8 + (widget.index / 8).toInt()) % 2 == 0
+          ? Colors.grey[300]
+          : Colors.grey[600],
+      child: widget.piece != null ? Image.asset(widget.piece!.imagePath) : null,
+    );
+  }
+}
---
+++ lib/helper_methods/square_selector.dart
@@ -0,0 +1,11 @@
+class ChessPiece {
+  final String pieceName;
+  final bool isWhite;
+  final String imagePath;
+
+  ChessPiece({
+    required this.pieceName,
+    required this.isWhite,
+    required this.imagePath,
+  });
+}
